cbf26a8ca2020771dc294241d67c6d6d310bbb26
tkdberger <tkdberger@gmail.com>
2016-03-25 00:59:22 -0400

    set the colspan of the stupid nav table
    automagically

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
 //initialize variables for some basic page controller
 //think RobotMap.h
-var numberOfNavLinks;
-numberOfNavLinks = 5
-
-alert('Hello World!');
-alert('This is what happens when I learn javascript.');
+var numberOfNavLinks = "5";
+function helloworld() {
+	alert("Hello World!");
+	alert("This is what happens when I learn javascript.");
+}
+function youIdiot() {
+	alert("* Y O U   I D I O T ."); 
+	//return false;
+}
+function bodyInit () {
+	document.getElementById("nav").setAttribute("colspan", numberOfNavLinks);
+	alert("My body is ready");
+}
