087997c68eed940b758e23eb9013fb97d067edb9
tkdberger <tkdberger@gmail.com>
2016-03-25 08:30:55 -0400

    ruse cruise

--- javascript.js
+++ javascript.js
@@ -1,15 +1,17 @@
 //initialize variables for some basic page controller
 //think RobotMap.h
 var numberOfNavLinks = "5";
+var audio = new Audio('sounds/Flowey_bosslaugh.ogg');
 function helloworld() {
 	alert("Hello World!");
 	alert("This is what happens when I learn javascript.");
 }
 function youIdiot() {
-	alert("* Y O U   I D I O T ."); 
-	//return false;
+	alert("* Y O U   I D I O T .");
+	audio.play();
+	return false;
 }
 function bodyInit () {
 	document.getElementById("nav").setAttribute("colspan", numberOfNavLinks);
-	alert("My body is ready");
+	//alert("My body is ready");
 }
