3965ce00fdb1328a310b639c06c2af447cff93c5
tkdberger <tkdberger@gmail.com>
2016-03-24 21:32:15 -0400

    Flowey has invaded my code
    pls to halp

--- javascript.js
+++ javascript.js
@@ -1,2 +1,7 @@
+//initialize variables for some basic page controller
+//think RobotMap.h
+var numberOfNavLinks;
+numberOfNavLinks = 5
+
 alert('Hello World!');
 alert('This is what happens when I learn javascript.');
