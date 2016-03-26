49ed1181914c2e08e95c3021a030a4c657556ded
tkdberger <tkdberger@gmail.com>
2016-03-26 09:53:16 -0400

    Add damage indicator number things
    That's numberwang....?

--- javascript.js
+++ javascript.js
@@ -1,54 +1,63 @@
 //initialize variables for some basic page controller
 //think RobotMap.h
+var isPlayerTurn = true;
+var ruseHealth = 200;
+
 var numberOfNavLinks = "5";
 var audio = new Audio('sounds/Flowey_bosslaugh.ogg');
-function helloworld() {
-	alert("Hello World!");
-	alert("This is what happens when I learn javascript.");
-}
 function youIdiot() {
 	audio.play();
 	alert("* Y O U   I D I O T .");
 	location.assign("ruse.html");
 }
 //ruse fight scene
-
 function fight() {
-    //send the user...somewhere.
-    location.assign("http://reddit.com/r/fitemeirl");
+    if(isPlayerTurn) {
+        //send the user...somewhere.
+        var attackPower = Math.floor((Math.random() * 25) + 1);
+        ruseHealth = ruseHealth - attackPower;
+        isPlayerTurn = false;
+    }
 }
 function mercy() {
-    //send user home
-    location.assign("index.html");
+    if (isPlayerTurn) {
+        //send user home
+        location.assign("index.html");
+        isPlayerTurn = false;
+   }
 }
 function call() {
-    clearElement("dialogContainer");
-    var dialogBox = document.getElementById("dialogContainer");
-    var newLine = document.createElement("li");
-    var whatToSay = document.createTextNode("You called for help...");
-    newLine.appendChild(whatToSay);
-    newLine.setAttribute("class", "dialog");
-    dialogBox.appendChild(newLine);
-    //////////////////////////////
-    newLine = document.createElement("li");
-    whatToSay = document.createTextNode("But nobody came.");
-    newLine.appendChild(whatToSay);
-    newLine.setAttribute("class", "dialog");
-    dialogBox.appendChild(newLine);
-    /////////////////////////////
-    window.setTimeout(keepCruising, 5000);
+    if(isPlayerTurn) {
+        clearElement("dialogContainer");
+        var dialogBox = document.getElementById("dialogContainer");
+        var newLine = document.createElement("li");
+        var whatToSay = document.createTextNode("You called for help...");
+        newLine.appendChild(whatToSay);
+        newLine.setAttribute("class", "dialog");
+        dialogBox.appendChild(newLine);
+        //////////////////////////////
+        newLine = document.createElement("li");
+        whatToSay = document.createTextNode("But nobody came.");
+        newLine.appendChild(whatToSay);
+        newLine.setAttribute("class", "dialog");
+        dialogBox.appendChild(newLine);
+        /////////////////////////////
+        window.setTimeout(keepCruising, 3000);
+    }
 }
 function eat() {
-    clearElement("dialogContainer");
-    var dialogBox = document.getElementById("dialogContainer");
-    var newLine = document.createElement("li");
-    var whatToSay = document.createTextNode("You ate something...this doesn't seem wise at the moment.");
-    newLine.appendChild(whatToSay);
-    newLine.setAttribute("class", "dialog");
-    dialogBox.appendChild(newLine);
-    //////////////////////////////
-    window.setTimeout(keepCruising, 5000);
-    /////////////////////////////
+    if(isPlayerTurn) {
+        clearElement("dialogContainer");
+        var dialogBox = document.getElementById("dialogContainer");
+        var newLine = document.createElement("li");
+        var whatToSay = document.createTextNode("You ate something...this doesn't seem wise at the moment.");
+        newLine.appendChild(whatToSay);
+        newLine.setAttribute("class", "dialog");
+        dialogBox.appendChild(newLine);
+        //////////////////////////////
+        window.setTimeout(keepCruising, 3000);
+        /////////////////////////////
+    }
 }
 function keepCruising() {
     clearElement("dialogContainer");
@@ -58,7 +67,23 @@ function keepCruising() {
     newLine.appendChild(whatToSay);
     newLine.setAttribute("class", "dialog");
     dialogBox.appendChild(newLine);
+    isPlayerTurn = false;
+    aiTurn();
 }
+
+function aiTurn() {
+    //nothing as of yet
+    alert("noai");
+    isPlayerTurn = true;
+    var dialogBox = document.getElementById("dialogContainer");
+    var newLine = document.createElement("li");
+    var whatToSay = document.createTextNode("Crusin' like nobody's business!");
+    newLine.appendChild(whatToSay);
+    newLine.setAttribute("class", "dialog");
+    dialogBox.appendChild(newLine);
+}
+
+//////////////////////////////////////////////////////////
 
 function bodyInit() {
 	document.getElementById("nav").setAttribute("colspan", numberOfNavLinks);
